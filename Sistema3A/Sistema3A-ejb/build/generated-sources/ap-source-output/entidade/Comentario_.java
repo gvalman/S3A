8640abb48e9f8adc5c5c7ef483de7036c41e66aa
gvalman <germanj@ibest.com.br>
2015-11-09 10:18:55 -0200

    Aplicando Mensagens, CSS e JS

--- Sistema3A/Sistema3A-ejb/build/generated-sources/ap-source-output/entidade/Comentario_.java
+++ Sistema3A/Sistema3A-ejb/build/generated-sources/ap-source-output/entidade/Comentario_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-07T17:54:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-09T10:16:10")
 @StaticMetamodel(Comentario.class)
 public class Comentario_ { 
 
